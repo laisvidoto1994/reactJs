5424e39264cf5c65ef55030d485ead2e71a68c65
laisvidoto1994 <lais.vidoto@gmail.com>
2018-11-14 11:04:25 -0200

    add

--- yt-react/src/components/counters.jsx
+++ yt-react/src/components/counters.jsx
@@ -10,15 +10,49 @@ class Counters extends Component {
             { id: 3, value: 0 },
             { id: 4, value: 0 }
         ]
-    }
+    };
+    
+    handleDelete = (counterId) =>{
+        console.log("handleDelete ",counterId);
+        const counters = this.state.counters.filter( c=> c.id !== counterId );
+        this.setState({ counters: counters });
+    }; 
+
+    handleReset= () => {
+        const counters = this.state.counters.map( c=> { 
+            c.value = 0;
+            return c;
+        });
+
+        this.setState({ counters: counters });
+    }; 
 
     render() {  
       return (
         <div> 
+             <button 
+              className="espacamento3 corAzul" 
+              onClick={ this.handleReset}
+             >
+              Reset
+            </button> 
+
+            {/*
             { this.state.counters.map( counter=> (
-                <Counter key={ counter.id } value={ counter.value } selected={ true } id={ counter.id }>   
+                <Counter key={ counter.id } onDelete={ this.handleDelete } value={ counter.value } id={ counter.id } selected={ counter.selected }>   
                 </Counter> 
             ) )}
+            */}
+
+            { this.state.counters.map( counter=> (
+                <Counter 
+                    key={ counter.id }  
+                    onDelete={ this.handleDelete } 
+                    counter={ counter }                    
+                >   
+                </Counter> 
+            ) )}
+
         </div>
       );
     }
