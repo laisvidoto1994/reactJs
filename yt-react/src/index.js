86fbee94c6b7a171d25748b2b6e0ee5f9258211e
laisvidoto1994 <lais.vidoto@gmail.com>
2018-11-14 11:19:22 -0200

    add

--- yt-react/src/index.js
+++ yt-react/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
- 
+
+// dois novos componentes criados Counters e Counter
 import Counters  from "./components/counters";
 import Counter  from "./components/counter";
 
+//<Counters />, é que eu quero que seja renderizado como se fosse o index principal
+//'root'-> será o id do elemento que ele irá procurar no arquivo de index.html e renderizar na tela o conteudo de <Counters />
 ReactDOM.render(<Counters />, document.getElementById('root'));
 
 serviceWorker.unregister();
